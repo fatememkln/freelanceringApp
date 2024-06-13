75464dbff524d0811810e8b9067b9e0751a4c8bf
fatememkln <fateme.mkln@gmail.com>
2024-06-13 22:24:04 +0330

    Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Auth from "./pages/Auth";
+
 function App() {
-  return <div className="text-3xl font-bold">پروژه اول</div>;
+  return (
+    <div className="container xl:max-w-screen-xl ">
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
